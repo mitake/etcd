1bb891cea15c34f0a5590d220d37dbfabf2231b7
Hitoshi Mitake <mitake.hitoshi@lab.ntt.co.jp>
2016-10-21 17:30:00 +0900

    wip

--- tools/functional-tester/docker/dummy.sh
+++ tools/functional-tester/docker/dummy.sh
@@ -1,9 +1,10 @@
 #! /bin/sh
 
 if [ "$1" = "enable" ]; then
-    curl -X POST '172.17.0.1:10080/api/v3/ctl/enableOrchestration'
+    curl -X POST '172.17.0.1:10080/api/v3/control?op=enableOrchestration'
 elif [ "$1" = "disable" ]; then
-    curl -X POST '172.17.0.1:10080/api/v3/ctl/disableOrchestration'
+    curl -X POST '172.17.0.1:10080/api/v3/control?op=disableOrchestration'
+    
 else
     echo "invalid argument: " $1
     exit 1
