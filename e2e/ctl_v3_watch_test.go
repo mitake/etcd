ee5c1f94dfdad3f4f7f249df7c462cdaca98d826
Hitoshi Mitake <mitake.hitoshi@lab.ntt.co.jp>
2017-04-25 14:49:55 +0900

    e2e: add a new test case for protecting watch with auth

--- e2e/ctl_v3_watch_test.go
+++ e2e/ctl_v3_watch_test.go
@@ -86,13 +86,19 @@ func watchTest(cx ctlCtx) {
 	}
 }
 
-func ctlV3Watch(cx ctlCtx, args []string, kvs ...kv) error {
+func setupWatchArgs(cx ctlCtx, args []string) []string {
 	cmdArgs := append(cx.PrefixArgs(), "watch")
 	if cx.interactive {
 		cmdArgs = append(cmdArgs, "--interactive")
 	} else {
 		cmdArgs = append(cmdArgs, args...)
 	}
+
+	return cmdArgs
+}
+
+func ctlV3Watch(cx ctlCtx, args []string, kvs ...kv) error {
+	cmdArgs := setupWatchArgs(cx, args)
 
 	proc, err := spawnCmd(cmdArgs)
 	if err != nil {
@@ -115,4 +121,26 @@ func ctlV3Watch(cx ctlCtx, args []string, kvs ...kv) error {
 		}
 	}
 	return proc.Stop()
+}
+
+func ctlV3WatchFailPerm(cx ctlCtx, args []string) error {
+	cmdArgs := setupWatchArgs(cx, args)
+
+	proc, err := spawnCmd(cmdArgs)
+	if err != nil {
+		return err
+	}
+
+	if cx.interactive {
+		wl := strings.Join(append([]string{"watch"}, args...), " ") + "\r"
+		if err = proc.Send(wl); err != nil {
+			return err
+		}
+	}
+
+	_, err = proc.Expect("permission denied")
+	if err != nil {
+		return err
+	}
+	return proc.Close()
 }
